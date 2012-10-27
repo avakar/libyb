74ba6abdb2e9f01f4626c7df3f814b648aad83b5
Martin Vejnár <avakar@ratatanek.cz>
2012-10-27 15:26:49 +0200

    Fixed stream::write_all.

--- libyb/async/stream.cpp
+++ libyb/async/stream.cpp
@@ -14,7 +14,7 @@ task<void> stream::read_all(uint8_t * buffer, size_t size)
 task<void> stream::write_all(uint8_t const * buffer, size_t size)
 {
 	return this->write(buffer, size).then([this, buffer, size](size_t transferred) -> task<void> {
-		if (size == transferred)
+		if (size != transferred)
 			return this->write_all(buffer + transferred, size - transferred);
 		else
 			return make_value_task();
