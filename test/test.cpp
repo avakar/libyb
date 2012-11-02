a7e0e0d3b2bd1f95544cdb644c1e1ec0dc4e2529
Martin Vejnár <avakar@ratatanek.cz>
2012-11-02 23:25:28 +0100

    Full support for tags in tests.

--- test/test.cpp
+++ test/test.cpp
@@ -12,6 +12,39 @@ test_registrar::test_registrar(void (*testfn)(), char const * name, char const *
 	g_reg = this;
 }
 
+bool test_registrar::is_eligible(std::set<yb::string_ref> const & args) const
+{
+	bool one_selected = false;
+
+	if (args.find(this->name) != args.end())
+		one_selected = true;
+
+	char const * first = this->tags;
+	for (char const * cur = first;; ++cur)
+	{
+		if (*cur != ' ' && *cur != 0)
+			continue;
+
+		if (first != cur)
+		{
+			bool is_required = first[0] == '+';
+
+			if (is_required && args.find(yb::string_ref(first, cur)) == args.end())
+				return false;
+
+			if (!is_required && args.find(yb::string_ref(first, cur)) != args.end())
+				one_selected = true;
+		}
+
+		if (*cur == 0)
+			break;
+
+		first = cur + 1;
+	}
+
+	return one_selected || args.empty();
+}
+
 void run_tests()
 {
 	run_tests(0, 0);
@@ -19,13 +52,13 @@ void run_tests()
 
 void run_tests(int argc, char const * const * argv)
 {
-	std::set<std::string> cmdline_tests;
+	std::set<yb::string_ref> cmdline_tests;
 	for (int i = 1; i < argc; ++i)
 		cmdline_tests.insert(argv[i]);
 
 	for (test_registrar * reg = g_reg; reg != 0; reg = reg->next)
 	{
-		if (!cmdline_tests.empty() && cmdline_tests.find(reg->name) == cmdline_tests.end())
+		if (!reg->is_eligible(cmdline_tests))
 			continue;
 
 		std::cout << reg->name << std::endl;
--- test/test.h
+++ test/test.h
@@ -1,6 +1,14 @@
+#ifndef LIBTESTS_TEST_H
+#define LIBTESTS_TEST_H
+
+#include <set>
+#include "../libyb/vector_ref.hpp"
+
 struct test_registrar
 {
 	test_registrar(void (*testfn)(), char const * name, char const * file, int line, char const * tags);
+
+	bool is_eligible(std::set<yb::string_ref> const & args) const;
 
 	void (*testfn)();
 	char const * name;
@@ -17,3 +25,5 @@ struct test_registrar
 
 void run_tests();
 void run_tests(int argc, char const * const * argv);
+
+#endif // LIBTESTS_TEST_H
