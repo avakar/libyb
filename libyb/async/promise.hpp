a4b930f20f7677d5bcb2f19477b2d5ef4f5b764c
Martin Vejnár <avakar@ratatanek.cz>
2012-11-02 21:50:26 +0100

    Fixed yb::promise to support <void> specialization.

--- libyb/async/promise.hpp
+++ libyb/async/promise.hpp
@@ -7,27 +7,86 @@
 
 namespace yb {
 
+namespace detail {
+
 template <typename T>
-class promise
+class promise_task_impl
+	: public task_base<T>
 {
 public:
-	promise()
+	explicit promise_task_impl(shared_circular_buffer<task_result<T>, 1> * buffer)
+		: m_buffer(buffer)
+	{
+		m_buffer->addref();
+	}
+
+	~promise_task_impl()
+	{
+		if (m_buffer)
+			m_buffer->release();
+	}
+
+	void cancel(cancel_level_t) throw()
+	{
+		if (m_buffer && m_buffer->empty())
+		{
+			m_buffer->release();
+			m_buffer = 0;
+		}
+	}
+
+	task_result<T> cancel_and_wait() throw()
+	{
+		this->cancel(cancel_level_hard);
+
+		if (m_buffer)
+			return task_result<T>(m_buffer->front());
+		else
+			return task_result<T>(std::copy_exception(std::runtime_error("cancelled")));
+	}
+
+	void prepare_wait(task_wait_preparation_context & ctx)
+	{
+		if (!m_buffer || !m_buffer->empty())
+			ctx.set_finished();
+	}
+
+	task<T> finish_wait(task_wait_finalization_context &) throw()
+	{
+		if (m_buffer)
+			return async::result(m_buffer->front());
+		else
+			return async::raise<T>(std::runtime_error("cancelled"));
+	}
+
+private:
+	shared_circular_buffer<task_result<T>, 1> * m_buffer;
+
+	promise_task_impl(promise_task_impl const &);
+	promise_task_impl & operator=(promise_task_impl const &);
+};
+
+template <typename T>
+class promise_base
+{
+public:
+	promise_base()
 		: m_buffer(new shared_circular_buffer<task_result<T>, 1>())
 	{
 	}
 
-	promise(promise const & o)
+	promise_base(promise_base const & o)
 		: m_buffer(o.m_buffer)
 	{
 		m_buffer->addref();
 	}
 
-	~promise()
+	~promise_base()
 	{
 		m_buffer->release();
 	}
 
-	promise & operator=(promise const & o)
+	promise_base & operator=(promise_base const & o)
 	{
 		o.m_buffer->addref();
 		m_buffer->release();
@@ -35,6 +94,29 @@ public:
 		return *this;
 	}
 
+	task<T> wait_for() const
+	{
+		return protect([this] {
+			return task<T>(new promise_task_impl<T>(m_buffer));
+		});
+	}
+
+	friend task<T> wait_for(promise_base const & p)
+	{
+		return p.wait_for();
+	}
+
+protected:
+	shared_circular_buffer<task_result<T>, 1> * m_buffer;
+};
+
+} // namespace detail
+
+template <typename T>
+class promise
+	: public detail::promise_base<T>
+{
+public:
 	void set_value(T && t) const
 	{
 		m_buffer->push_back(task_result<T>(std::move(t)));
@@ -44,72 +126,17 @@ public:
 	{
 		m_buffer->push_back(task_result<T>(t));
 	}
+};
 
-	task<T> get() const
+template <>
+class promise<void>
+	: public detail::promise_base<void>
+{
+public:
+	void set_value() const
 	{
-		return protect([this] {
-			return task<T>(new task_impl(m_buffer));
-		});
+		m_buffer->push_back(task_result<void>());
 	}
-
-private:
-	class task_impl
-		: public task_base<T>
-	{
-	public:
-		explicit task_impl(shared_circular_buffer<task_result<T>, 1> * buffer)
-			: m_buffer(buffer)
-		{
-			m_buffer->addref();
-		}
-
-		~task_impl()
-		{
-			if (m_buffer)
-				m_buffer->release();
-		}
-
-		void cancel(cancel_level_t) throw()
-		{
-			if (m_buffer && m_buffer->empty())
-			{
-				m_buffer->release();
-				m_buffer = 0;
-			}
-		}
-
-		task_result<T> cancel_and_wait() throw()
-		{
-			this->cancel(cancel_level_hard);
-
-			if (m_buffer)
-				return task_result<T>(m_buffer->front());
-			else
-				return task_result<T>(std::copy_exception(std::runtime_error("cancelled")));
-		}
-
-		void prepare_wait(task_wait_preparation_context & ctx)
-		{
-			if (!m_buffer || !m_buffer->empty())
-				ctx.set_finished();
-		}
-
-		task<T> finish_wait(task_wait_finalization_context &) throw()
-		{
-			if (m_buffer)
-				return async::result(m_buffer->front());
-			else
-				return async::raise<T>(std::runtime_error("cancelled"));
-		}
-
-	private:
-		shared_circular_buffer<task_result<T>, 1> * m_buffer;
-
-		task_impl(task_impl const &);
-		task_impl & operator=(task_impl const &);
-	};
-
-	shared_circular_buffer<task_result<T>, 1> * m_buffer;
 };
 
 } // namespace yb
