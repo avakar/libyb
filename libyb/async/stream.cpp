208ff6b53809dffd492e628e220ad58a64d20fde
Martin Vejnár <avakar@ratatanek.cz>
2013-12-29 19:32:18 +0100

    Added stream::read_all for chunked read.

--- libyb/async/stream.cpp
+++ libyb/async/stream.cpp
@@ -14,6 +14,21 @@ task<void> stream::read_all(uint8_t * buffer, size_t size)
 	});
 }
 
+task<void> stream::read_all(std::function<bool(buffer_ref const & chunk)> const & handler, size_t max_chunk_size)
+{
+	if (max_chunk_size == 0)
+		max_chunk_size = 16*1024;
+	size_t const buffer_size = (std::min)(max_chunk_size, (size_t)(16 * 1024));
+	std::shared_ptr<std::vector<uint8_t> > buf = std::make_shared<std::vector<uint8_t> >(buffer_size);
+	return loop(async::value((size_t)-1), [this, handler, buf](size_t r, cancel_level cl) -> task<size_t> {
+		if (cl >= cl_quit)
+			return async::raise<size_t>(task_cancelled(cl));
+		if (r != (size_t)-1 && !handler(buffer_ref(buf->data(), r)))
+			return nulltask;
+		return this->read(buf->data(), buf->size());
+	});
+}
+
 task<void> stream::write_all(uint8_t const * buffer, size_t size)
 {
 	return loop_with_state<size_t, size_t>(async::value((size_t)0), 0, [this, buffer, size](size_t r, size_t & st, cancel_level cl) -> task<size_t> {
--- libyb/async/stream.hpp
+++ libyb/async/stream.hpp
@@ -2,7 +2,9 @@
 #define LIBYB_ASYNC_STREAM_HPP
 
 #include "task.hpp"
+#include "../vector_ref.hpp"
 #include <stdint.h>
+#include <functional>
 
 namespace yb {
 
@@ -13,7 +15,9 @@ public:
 	virtual task<size_t> write(uint8_t const * buffer, size_t size) = 0;
 
 	task<void> read_all(uint8_t * buffer, size_t size);
+	task<void> read_all(std::function<bool(buffer_ref const & chunk)> const & handler, size_t max_chunk_size = 0);
 	task<void> write_all(uint8_t const * buffer, size_t size);
+
 };
 
 task<void> copy(stream & sink, stream & source, size_t buffer_size = 256);
