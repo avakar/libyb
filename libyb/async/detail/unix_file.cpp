db4e171767023039610a0192fc8875714ef0f340
Martin Vejnár <avakar@ratatanek.cz>
2014-03-09 14:34:20 +0100

    Implemented yb::file on Linux.

---
+++ libyb/async/detail/unix_file.cpp
@@ -0,0 +1,217 @@
+#include "../file.hpp"
+#include "linux_fdpoll_task.hpp"
+#include "unix_file.hpp"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+using namespace yb;
+
+class unix_system_error
+	: public std::runtime_error
+{
+public:
+	explicit unix_system_error(int error_number)
+		: std::runtime_error(std::string()), m_errno(error_number)
+	{
+	}
+
+	int error_number() const
+	{
+		return m_errno;
+	}
+
+private:
+	int m_errno;
+};
+
+namespace {
+
+struct writer_t
+{
+public:
+	writer_t(int fd, buffer_ref buf)
+		: m_fd(fd), m_buf(buf)
+	{
+	}
+
+	task<size_t> operator()()
+	{
+		for (;;)
+		{
+			ssize_t r = ::write(m_fd, m_buf.data(), m_buf.size());
+			if (r == -1)
+			{
+				int e = errno;
+
+				if (e == EINTR)
+					continue;
+
+				if (e == EAGAIN || e == EWOULDBLOCK)
+				{
+					return make_linux_pollfd_task(m_fd, POLLOUT, [](cancel_level cl) {
+						return cl < cl_abort;
+					}).ignore_result().then(*this);
+				}
+
+				return async::raise<size_t>(unix_system_error(e));
+			}
+
+			return async::value((size_t)r);
+		}
+	}
+
+private:
+	int m_fd;
+	buffer_ref m_buf;
+};
+
+struct reader_t
+{
+public:
+	reader_t(int fd, buffer && buf)
+		: m_fd(fd), m_buf(std::move(buf))
+	{
+	}
+
+	task<buffer_view> operator()()
+	{
+		for (;;)
+		{
+			ssize_t r = ::read(m_fd, m_buf.data(), m_buf.size());
+			if (r == -1)
+			{
+				int e = errno;
+
+				if (e == EINTR)
+					continue;
+
+				if (e == EAGAIN || e == EWOULDBLOCK)
+				{
+					return make_linux_pollfd_task(m_fd, POLLOUT, [](cancel_level cl) {
+						return cl < cl_abort;
+					}).ignore_result().then(std::move(*this));
+				}
+
+				return async::raise<buffer_view>(unix_system_error(e));
+			}
+
+			return async::value(buffer_view(std::move(m_buf), r));
+		}
+	}
+
+private:
+	int m_fd;
+	buffer && m_buf;
+};
+
+task<buffer_view> fetch_and_read(int fd, buffer_policy policy, size_t max_size)
+{
+	return policy.fetch(1, max_size).then([fd, max_size](buffer buf) {
+		return reader_t(fd, std::move(buf))();
+	});
+}
+
+}
+
+task<buffer_view> yb::detail::read_linux_fd(int fd, buffer_policy policy, size_t max_size)
+{
+	struct pollfd pf = {};
+	pf.fd = fd;
+	pf.events = POLLIN;
+
+	for (;;)
+	{
+		int r = ::poll(&pf, 1, 0);
+		if (r == -1)
+		{
+			int e = errno;
+			if (e == EINTR)
+				continue;
+			return async::raise<buffer_view>(unix_system_error(e));
+		}
+
+		struct cont_t
+		{
+		public:
+			cont_t(int fd, buffer_policy policy, size_t max_size)
+				: m_fd(fd), m_policy(std::move(policy)), m_max_size(max_size)
+			{
+			}
+
+			task<buffer_view> operator()(short revents)
+			{
+				(void)revents;
+				return fetch_and_read(m_fd, std::move(m_policy), m_max_size);
+			}
+
+		private:
+			int m_fd;
+			buffer_policy m_policy;
+			size_t m_max_size;
+		};
+
+		if (r == 0)
+		{
+			return make_linux_pollfd_task(fd, POLLIN, [](cancel_level cl) {
+				return cl < cl_abort;
+			}).then(cont_t(fd, std::move(policy), max_size));
+		}
+
+		return fetch_and_read(fd, std::move(policy), max_size);
+	}
+}
+
+task<size_t> yb::detail::write_linux_fd(int fd, buffer_ref buf)
+{
+	return writer_t(fd, buf)();
+}
+
+struct file::impl
+{
+	int fd;
+};
+
+file::file(yb::string_ref const & fname)
+{
+	std::string zfname = fname;
+
+	std::unique_ptr<impl> pimpl(new impl());
+	pimpl->fd = open(zfname.c_str(), O_CLOEXEC | O_NONBLOCK | O_RDWR);
+	if (pimpl->fd == -1)
+		throw unix_system_error(errno);
+	m_pimpl = pimpl.release();
+}
+
+file::~file()
+{
+	this->clear();
+}
+
+void file::clear()
+{
+	if (m_pimpl)
+	{
+		close(m_pimpl->fd);
+		delete m_pimpl;
+		m_pimpl = 0;
+	}
+}
+
+file::file_size_t file::size() const
+{
+	struct stat st;
+	if (fstat(m_pimpl->fd, &st) == -1)
+		throw unix_system_error(errno);
+	return st.st_size;
+}
+
+task<buffer_view> file::read(buffer_policy policy, size_t max_size)
+{
+	return detail::read_linux_fd(m_pimpl->fd, std::move(policy), max_size);
+}
+
+task<size_t> file::write(buffer_ref buf)
+{
+	return detail::write_linux_fd(m_pimpl->fd, buf);
+}
---
+++ libyb/async/detail/unix_file.hpp
@@ -0,0 +1,16 @@
+#ifndef LIBYB_ASYNC_DETAIL_UNIX_FILE_HPP
+#define LIBYB_ASYNC_DETAIL_UNIX_FILE_HPP
+
+#include "../task.hpp"
+#include "../../buffer.hpp"
+
+namespace yb {
+namespace detail {
+
+task<buffer_view> read_linux_fd(int fd, buffer_policy policy, size_t max_size);
+task<size_t> write_linux_fd(int fd, buffer_ref buf);
+
+}
+}
+
+#endif // LIBYB_ASYNC_DETAIL_UNIX_FILE_HPP
